4dd8bf89b5d9eb2ba982e1f2966b7022f83f0ede
FernandoM52 <fernandomartins.contato@outlook.com>
2023-08-19 23:24:35 -0300

    feat/ Get tweets - Return all tweets for user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/users';
+import { Tweet } from './entities/tweets';
 
 @Injectable()
 export class AppService {
+  private users: User[];
+  private tweets: Tweet[];
+
+  constructor() {
+    this.users = [];
+    this.tweets = [];
+  }
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getTweets(): Tweet[] {
+    return this.tweets;
   }
 }
