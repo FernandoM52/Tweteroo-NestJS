8044f0ea18434d303d6e30609b7f4804acc209cb
FernandoM52 <fernandomartins.contato@outlook.com>
2023-08-20 00:14:03 -0300

    feat/ signUp - Now users can register

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Tweet } from './entities/tweets';
+import { CreateUserDto } from './dtos/create-user.dtos';
+import { User } from './entities/users'; //retirar dps
 
 @Controller()
 export class AppController {
@@ -11,8 +13,19 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  @Post("sign-up")
+  @HttpCode(200)
+  signUp(@Body() body: CreateUserDto) {
+    return this.appService.signUp(body);
+  }
+
   @Get("tweets")
   getTweets(): Tweet[] {
     return this.appService.getTweets();
+  }
+
+  @Get("users")
+  getUsers(): User[] {
+    return this.appService.getUsers();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entities/users';
 import { Tweet } from './entities/tweets';
+import { CreateUserDto } from './dtos/create-user.dtos';
 
 @Injectable()
 export class AppService {
@@ -16,7 +17,18 @@ export class AppService {
     return 'Hello World!';
   }
 
+  signUp(body: CreateUserDto) {
+    const { username, avatar } = body;
+    this.users.push(new User(username, avatar));
+  }
+
   getTweets(): Tweet[] {
+    // const lastFifteenTweets = this.tweets.slice(-15);
+    // return lastFifteenTweets;
     return this.tweets;
+  }
+
+  getUsers(): User[] {
+    return this.users;
   }
 }
